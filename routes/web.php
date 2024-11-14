5d4a9c67f22c8ec1a7bd2a9cd2a82eec9c832a0f
Gavin Murambadoro <developer.gavin@gmail.com>
2024-11-14 10:22:52 +0200

    Installed and configured fortify for authentication

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', fn() => view('home'))
+        ->name('home');
+});
+
