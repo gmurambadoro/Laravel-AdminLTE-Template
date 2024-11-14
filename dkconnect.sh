5070af83c6de3792e2c22a3ee98e16fcc9041d4c
Gavin Murambadoro <developer.gavin@gmail.com>
2024-11-14 11:01:53 +0200

    Added Mailhog configuration

--- dkconnect.sh
+++ dkconnect.sh
@@ -4,5 +4,6 @@ source docker.env
 
 echo "Web:             http://localhost:$DC_APP_PORT"
 echo "phpMyAdmin:      http://localhost:$DC_PMA_PORT"
+echo "Mailhog:         http://localhost:$DC_MAILHOG_WEBUI_PORT"
 
 docker exec -it "$DC_APP_NAME" bash
