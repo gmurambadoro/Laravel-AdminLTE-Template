a42d280aad5710ed463b564f40f1c9a8fb30a125
Gavin Murambadoro <developer.gavin@gmail.com>
2024-11-14 10:11:04 +0200

    Added base layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-layouts.app>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
+    <x-slot:breadcrumbs>
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+    </x-slot:breadcrumbs>
+
+    <p>Hello, world!</p>
+</x-layouts.app>
